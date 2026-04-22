9e2b6916e7c04391a2fccf7b964d2d616be1129b
saLman252158 <mdsalmansajid2@gmail.com>
2026-04-22 14:59:21 +0600

    adminhome

---
+++ crude operation/adminhome.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Admin Home</title>
+</head>
+<body>
+    <h1>Admin Home</h1>
+    <p>Welcome, Admin!</p>
+    <br><br>
+
+<table border="1" cellspacing="0" cellpadding="5">
+
+    <tr>
+        <th colspan="3">User List</th>
+    </tr>
+
+    <tr>
+        <td>User</td>
+        <td>Password</td>
+        <td>Action</td>
+    </tr>     
+</table>
+
+</body>
+</html>
